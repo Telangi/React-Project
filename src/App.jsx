0baf7d0ad71bc18785de97add75ce3fc93c13f4f
Your Name <vaishnavitelangi21@gmail.com>
2026-05-21 12:58:40 +0530

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
         <Route
           path="/"
           element={
-            <Navigate to="/Signup" />
+            <Navigate to="/signup" />
           }
         />
 
